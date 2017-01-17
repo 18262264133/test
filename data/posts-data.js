54b7c7d1c7cca971fe5e68e349f917b8aa47a749
xunxiaowei <xunxiaowei@126.com>
2017-01-17 22:42:36 +0800

    weixin-test

---
+++ data/posts-data.js
@@ -0,0 +1,56 @@
+var local_database = [
+    {
+        date: "Nov 25 2016",
+        title: "正是虾肥蟹壮时",
+        imgSrc: "/image/post/crab.png",
+        content: "有诗云：“九月圆脐十月尖，持螯饮酒菊花天。”诗里的“圆脐”为雌蟹，“尖”为雄蟹，是说农历九月雌蟹抱卵，蟹黄饱满，食之为佳；农历十月雄蟹脂膏丰腴，肉质嫩白，食之最妙。但不管怎样，菊黄时节，秋高气爽，正是美啖大闸蟹的最好时光。这段时间的餐桌上，作为美食，大闸蟹绝对是耀武扬威的主角，没有之一",
+        reading: "92",
+        collection: "65",
+        avatar: "/image/avatar/4.png",
+        postId: 0
+    },
+    {
+        date: "Dev 18 2016",
+        title: "比利.林恩的中场故事",
+        imgSrc: "/image/post/bl.png",
+        content: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        reading: "112",
+        collection: "96",
+        avatar: "/image/avatar/2.png",
+        postId: 1
+    },
+    {
+        date: "Jan 12 2017",
+        title: "小米雷军：想做真正的创业者，一定要有阿猫阿狗的觉悟",
+        imgSrc: "/image/post/cat.png",
+        content: "陷入低谷一年后，雷军想清楚了小米的下一步。雷军表示，主要的问题是大家用了10年（智能）手机以后，觉得手机的同质化很严重，“这一两年的手机行业，大家觉得创新到了瓶颈期。主要是这个问题导致（的）。”",
+        reading: "666",
+        collection: "66",
+        avatar: "/image/avatar/3.png",
+        postId: 2
+    },
+    {
+        date: "Jan 13 2017",
+        title: "当高铁遇上人脸识别：近期剃个光头可能认不出！",
+        imgSrc: "/image/post/sls.jpg",
+        content: "春运，每年春节前都会发生的大规模高交通客流物流运输的现象，也一直是媒体和诸多游子关注的一个焦点。今年的春运，一票难求的局面早就被一些研发抢票软件的公司预测到了，很多人虽早有准备，但依旧还是不那么容易就能拿到回家的车票！",
+        reading: "123",
+        collection: "82",
+        avatar: "/image/avatar/1.png",
+        postId: 3
+    },
+    {
+        date: "Jan 14 2017",
+        title: "“直播力”：80亿当量的加速度",
+        imgSrc: "/image/post/vr.png",
+        content: "关于直播的种种，在被称作“移动直播元年”的2016年已经被说了太多。事实上，直播行业本身便具有极强的不可预知性：比如曾经引得无数VC竞折腰的直播风口促成了轰轰烈烈的“千播大战”；但也是同一批VC在2016年末集",
+        reading: "987",
+        collection: "156",
+        avatar: "/image/avatar/5.png",
+        postId: 4
+    }
+]
+
+module.exports = {
+    postList: local_database
+}
---
+++ pages/posts/posts.js
@@ -0,0 +1,19 @@
+var postsData = require('../../data/posts-data.js');
+
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    this.setData({
+      posts_key: postsData.postList
+    })
+  },
+  toTap:function(event){
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail',
+    })
+  }
+})
